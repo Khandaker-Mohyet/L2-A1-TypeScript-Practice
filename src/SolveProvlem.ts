e5f65d257722e1aab97c6cd75b79e3c2efd9faee
Khandaker-Mohyet <mohyet008@gmail.com>
2025-05-08 01:56:12 +0600

    seventh doen

--- src/SolveProvlem.ts
+++ src/SolveProvlem.ts
@@ -111,4 +111,29 @@ const products = [
 ];
 
 getMostExpensiveProduct(products);  
-// Output: { name: "Bag", price: 50 }
+
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday
+}
+
+function getDayType(day: Day): string {
+ 
+  switch (day) {
+    case Day.Saturday:
+    case Day.Sunday:
+      return "Weekend";  
+    default:
+      return "Weekday";   
+  }
+}
+
+
+console.log(getDayType(Day.Monday));   
+console.log(getDayType(Day.Sunday));   
