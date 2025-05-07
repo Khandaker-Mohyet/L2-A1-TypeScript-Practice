169856033d85c4a45c8d824fb9e682a96ac952e3
Khandaker-Mohyet <mohyet008@gmail.com>
2025-05-08 00:31:57 +0600

    fourth doen

--- src/SolveProvlem.ts
+++ src/SolveProvlem.ts
@@ -37,3 +37,35 @@ function concatenateArrays<T>(...arrays: T[][]): T[]{
 
 concatenateArrays(["a", "b"], ["c"]);      
 concatenateArrays([1, 2], [3, 4], [5]);    
+
+
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  getInfo() {
+    // console.log(`"Make: ${this.make}, Year: ${this.year}"`)
+  }
+}
+
+class Car extends Vehicle{
+  private model: string;
+
+  constructor(make: string, year: number,model: string) {
+    super(make, year)
+    this.model = model;
+  }
+
+  getModel() {
+    // console.log(`"Model: ${this.model}"`)
+  }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+myCar.getInfo();  
+myCar.getModel();
